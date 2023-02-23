7e532da82c4be9ac977d8edf8116b58d56de765c
Samarth H Chinivar <samarthharish321@gmail.com>
2023-02-23 23:09:14 +0530

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import {Box, Button} from '@chakra-ui/react';
+import { Link} from '@chakra-ui/react';
+
+const Footer = () => (
+    <Box textAlign="center" p="5" color="gray.600" borderTop="1px" borderColor="gray.100">
+        &copy; 2023 Stark Estates, Inc.
+        <br/>
+        <Link href='https://samarth-portfolio-website.000webhostapp.com' target="_blank">Contact</Link>
+        
+    </Box>
+);
+
+export default Footer;
